fea6e1b339009ab751f5ffdc010cff15717dc79b
SobeninAlex <sobeninalex@yandex.ru>
2023-10-03 22:13:21 +0500

    add SendMessageLogic

--- app/src/main/java/com/example/messengerfirebase/view/ChatActivity.java
+++ app/src/main/java/com/example/messengerfirebase/view/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.example.messengerfirebase.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -10,11 +12,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.messengerfirebase.R;
 import com.example.messengerfirebase.adapters.MessagesAdapter;
 import com.example.messengerfirebase.model.Message;
 import com.example.messengerfirebase.model.User;
+import com.example.messengerfirebase.viewModel.ChatViewModel;
+import com.example.messengerfirebase.viewModel.ChatViewModelFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +38,10 @@ public class ChatActivity extends AppCompatActivity {
     private MessagesAdapter adapter;
 
     private String currentUserID;
+    private User otherUser;
+
+    private ChatViewModel viewModel;
+    private ChatViewModelFactory viewModelFactory;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,24 +49,44 @@ public class ChatActivity extends AppCompatActivity {
 
         initViews();
 
-        var user = (User) getIntent().getSerializableExtra(USER_EXTRA);
-        var userInfo = String.format("%s %s, %d", user.getName(), user.getLastNeme(), user.getAge());
-
+        otherUser = (User) getIntent().getSerializableExtra(USER_EXTRA);
+        currentUserID = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
+        var userInfo = String.format("%s %s, %d", otherUser.getName(), otherUser.getLastNeme(), otherUser.getAge());
         textViewTitle.setText(userInfo);
 
-        currentUserID = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
+        viewModelFactory = new ChatViewModelFactory(currentUserID, otherUser.getId());
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(ChatViewModel.class);
+
         adapter = new MessagesAdapter(currentUserID);
         recyclerViewMessages.setAdapter(adapter);
 
-        //тест
-        List<Message> messageList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            messageList.add(new Message("text" + i, user.getId(), currentUserID));
-        }
-        for (int i = 0; i < 10; i++) {
-            messageList.add(new Message("text" + i, currentUserID, user.getId() ));
-        }
-        adapter.setMessages(messageList);
+        observeViewModel();
+        imageViewSendMessage.setOnClickListener(view -> {
+            var textMessage = editTextMessage.getText().toString().trim();
+            if (!textMessage.isEmpty()) {
+                var message = new Message(textMessage, currentUserID, otherUser.getId());
+                viewModel.sendMessage(message);
+            }
+        });
+
+    }
+
+    private void observeViewModel() {
+        viewModel.getMessagesList().observe(this, messages -> {
+            adapter.setMessages(messages);
+        });
+
+        viewModel.getErrorMessage().observe(this, errorMessage -> {
+            if (errorMessage != null) {
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewModel.getMessageSend().observe(this, isSend -> {
+            if (isSend) {
+                editTextMessage.setText("");
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/example/messengerfirebase/viewModel/ChatViewModel.java
@@ -0,0 +1,121 @@
+package com.example.messengerfirebase.viewModel;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.messengerfirebase.model.Message;
+import com.example.messengerfirebase.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatViewModel extends ViewModel {
+
+    private MutableLiveData<List<Message>> messagesList = new MutableLiveData<>();
+    private MutableLiveData<Boolean> messageSend = new MutableLiveData<>();
+    private MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private MutableLiveData<User> otherUser = new MutableLiveData<>();
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference referenceUser = database.getReference("Users");
+    private DatabaseReference referenceMessages = database.getReference("Messages");
+
+    private String currentUserID;
+    private String otherUserID;
+
+    public ChatViewModel(String currentUserID, String otherUserID) {
+        this.currentUserID = currentUserID;
+        this.otherUserID = otherUserID;
+
+        //TODO -> возможно не пригодится
+        referenceUser.child(otherUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                var user = snapshot.getValue(User.class);
+                otherUser.setValue(user);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        //
+
+        referenceMessages.child(currentUserID).child(otherUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<Message> messages = new ArrayList<>();
+                for ( DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    var message = dataSnapshot.getValue(Message.class);
+                    messages.add(message);
+                }
+                messagesList.setValue(messages);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void sendMessage(Message message) {
+        //сохраняем сообщение в БД у отправителя и у получателя
+        referenceMessages
+                .child(message.getSenderID())
+                .child(message.getReceiverID())
+                .push()
+                .setValue(message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        referenceMessages
+                                .child(message.getReceiverID())
+                                .child(message.getSenderID())
+                                .push()
+                                .setValue(message)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        messageSend.setValue(true);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        errorMessage.setValue(e.getMessage());
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        errorMessage.setValue(e.getMessage());
+                    }
+                });
+    }
+
+    public LiveData<User> getOtherUser() {
+        return otherUser;
+    }
+    public LiveData<List<Message>> getMessagesList() {
+        return messagesList;
+    }
+    public LiveData<Boolean> getMessageSend() {
+        return messageSend;
+    }
+    public LiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+}
